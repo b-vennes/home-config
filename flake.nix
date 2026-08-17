7d694e49ffaa39e229102e8a79572d5fbe13811c
Branden <b.n.vennes@gmail.com>
2026-08-17 05:41:17 -0500

    initial commit with current home configuration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Personal computer home manager config";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          git-machete
+
+          nil
+        ];
+      };
+    });
+}
